ecf5d10ae05f19f5f2401975b7f295a37649a583
pico <pico@example.com>
2025-08-29 20:22:49 +0200

    Worm updates

--- code/08/docker/start.sh
+++ code/08/docker/start.sh
@@ -17,8 +17,8 @@ do
    ADDR="172.20.0.$i"
    NAME="victim$i"
    echo -n "\"$ADDR\"," >> host.h
-   #docker run --rm --net piconet --ip ${ADDR} --security-opt label=disable -v $PWD/snase:/opt/snase --name ${NAME} -d alpine /opt/snase/snase
-   docker run --rm --net piconet --ip ${ADDR} --security-opt label=disable -v $PWD/snase:/opt/snase --name ${NAME} -d alpine  /opt/snase/run-x86_64.sh
+   docker run --rm --net piconet --ip ${ADDR} --security-opt label=disable -v $PWD/snase:/opt/snase --name ${NAME} -d alpine /opt/snase/snase
+   #docker run --rm --net piconet --ip ${ADDR} --security-opt label=disable -v $PWD/snase:/opt/snase --name ${NAME} -d alpine  /opt/snase/run-x86_64.sh
 done
 echo  -n " NULL};" >> host.h 
 # Run the attacker machine
